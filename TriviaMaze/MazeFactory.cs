04d89bf59fd143ec8dd3d819b5c0c37d18c68f71
Grondhog <Sgronhovd@gmail.com>
2014-11-04 13:42:57 -0800

    Maze Factory
    Created the simple Maze Factory Class, with a make Maze class

---
+++ TriviaMaze/MazeFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TriviaMaze
+{
+    class MazeFacotry
+    {
+        public MazeFacotry()
+        {
+
+        }
+
+        public Room[,] makeMaze(int size)
+        {
+            Room[,] maze = new Room[size,size];
+            for(int i = 0; i < size; i++)
+            {
+                for(int j = 0; j < size; j++)
+                {
+                    maze[i, j] = new Room(i, j);
+                }
+            }//end outer loop
+            return maze;
+        }
+    }
+}
